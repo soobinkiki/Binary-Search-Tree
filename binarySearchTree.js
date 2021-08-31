0a86264c44aade8afa42ebf61f9708757f004ab3
Justin Park <77993327+soobinkiki@users.noreply.github.com>
2021-08-31 15:07:28 -0700

    hided first root design before pressing space for the first time

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -1,18 +1,31 @@
 
 let number = document.getElementById("number")
-
+let rootNumTitle = document.getElementById('rootnum-title')
 
 const numberMinMax = (min, max) => {
+    
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 const randomNumber = e => {
-    if (e.keyCode == 32) {
+    if (e.keyCode == 32) { // keyCode32 => space
         number.textContent = numberMinMax(-100, 100)
     }
+    console.log("number.innerText", typeof(number.innerText));
+    if(number.innerText == "") {
+        number.style.backgroundColor = "white"
+    } else {
+        number.style.backgroundColor = 'rgb(' + [15,218,184].join(',') + ')';
+        rootNumTitle.innerText = ""
+    }
+    // {number.innerText == "" ? number.style.backgroundColor = "white" : "red"}
+}
+
+const addNumber = () => {
+
 }
 
 const removeNumber = (e) => {
- 
+    
 }
 
 document.addEventListener('keypress', randomNumber)
