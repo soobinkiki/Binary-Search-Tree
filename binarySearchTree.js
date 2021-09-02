997e15136fb2304ca01b22003d0d09ad80e48df9
Justin Park <77993327+soobinkiki@users.noreply.github.com>
2021-09-01 20:17:50 -0700

    babel installed for jsx in javascript

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -1,37 +1,68 @@
 import { TreeCalculate } from './utils/utils.js'
+/** @jsx createElement */
+/*** @jsxFrag createFragment */
+
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+
 const bst = new TreeCalculate()
 
 let number = document.getElementById("number")
 let rootNumTitle = document.getElementById('rootnum-title')
 let leftnum = document.getElementById("left")
 let rightnum = document.getElementById("right")
-
+let leftright = document.querySelector(".leftright-container")
 const mainTree = data => {
 
+    // const render = () => {
+    //     <ul>{renderTree(data)}</ul>
+    // }
     
-    function renderTree(node) {
-        const treeStructure = (
-            <li key={value.data}>
-                {/* <mainTree data={value} /> */}
-            </li>
-        )
+    // function renderTree(node) {
+        const UsingFragment = () => (
+            <div>
+              <p>This is regular paragraph</p>
+              <div>
+                <p>This is a paragraph in a fragment</p>
+                <>
+                  <p>Hello</p>
+                </>
+                <ul>
+                  {[1, 2, 3].map((item) => (
+                    <li>{item}</li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          );
+        
+        leftright.appendChild(<UsingFragment />) // name?
+        
         return Object.entries(node).map(function ([key, value]) {
-          if (key === 'left' && typeof value === 'object') {
+        if (key === 'left' && typeof value === 'object') {
             if (value !== null) {
-              return treeStructure
+                return leftright
             }
             if (value === null) {
-              return null;
+                return null;
             }
-          } else if (key === 'right' && typeof value === 'object') {
+        } else if (key === 'right' && typeof value === 'object') {
             if (value !== null) {
-              return treeStructure
+                return leftright
             }
             if (value === null) {
-              return null;
+                return null;
             }
-          }
-        });
+        }
+    });
+    // }
+    // return console.log("hello");
 }
 
 const numberMinMax = (min, max) => {
@@ -43,7 +74,7 @@ const randomNumber = e => {
     }
     console.log("number.innerText", typeof(number.textContent));
     if(number.textContent == "") {
-        number.style.backgroundColor = "red"
+        number.style.backgroundColor = "white"
     } else {
         number.style.backgroundColor = 'rgb(' + [15,218,184].join(',') + ')';
         rootNumTitle.textContent = ""
@@ -54,8 +85,14 @@ const randomNumber = e => {
     // {number.innerText == "" ? number.style.backgroundColor = "white" : "red"}
 }
 
+
 const addNumber = () => {
-    bst.addNum(number.textContent)
+    var node = new Node(10);
+    node.left = new Node(5);
+    node.right = new Node(15);
+    bst.root = node
+    mainTree(bst.root)
+    // bst.addNum(number.textContent)
     // leftnum.textContent.numsStorage
     
 }
